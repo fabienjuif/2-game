ab33854b96dd4e6f4362c521928a7615eced609d
Fabien JUIF <fabien.juif@gmail.com>
2019-03-16 13:46:08 +0100

    :tada: first commit

---
+++ beta1/precache-manifest.cb23da62928edc05187460dad0260dfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c7a8fad03fc47610dca",
+    "url": "/2-game/beta1/static/js/main.1ff9e523.chunk.js"
+  },
+  {
+    "revision": "4cbb0d24351127adbd37",
+    "url": "/2-game/beta1/static/js/runtime~main.79d0428e.js"
+  },
+  {
+    "revision": "77a662f6fd99ba06a7af",
+    "url": "/2-game/beta1/static/js/2.2b10e47c.chunk.js"
+  },
+  {
+    "revision": "f763c98b3b1d5279feef0a2c2a61406e",
+    "url": "/2-game/beta1/static/media/bg.f763c98b.png"
+  },
+  {
+    "revision": "3f67354e9a67b4ab1fb87aecdfe11be8",
+    "url": "/2-game/beta1/index.html"
+  }
+];
