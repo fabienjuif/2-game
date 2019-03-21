6f8d8543a8921a152914b461e50c69a40bb48b67
Fabien JUIF <fabien.juif@gmail.com>
2019-03-21 14:34:37 +0100

    :sparkles: beta11

---
+++ beta11/precache-manifest.d6c89190690c9d70d9913047a0f6ff9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5ffead73e47bf810369",
+    "url": "/2-game/beta11/static/css/main.5aee2c60.chunk.css"
+  },
+  {
+    "revision": "a5ffead73e47bf810369",
+    "url": "/2-game/beta11/static/js/main.23625e18.chunk.js"
+  },
+  {
+    "revision": "b13b0c33903d48b5204a",
+    "url": "/2-game/beta11/static/js/runtime~main.c4dd3827.js"
+  },
+  {
+    "revision": "ec1c0a08df466da36488",
+    "url": "/2-game/beta11/static/js/2.2c0a8e29.chunk.js"
+  },
+  {
+    "revision": "4e829d28b9ec001d4a386c145641fdb7",
+    "url": "/2-game/beta11/index.html"
+  }
+];
